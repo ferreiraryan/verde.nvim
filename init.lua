4a4a901df9e048280ce83c8748b1c919abd5b1d0
ferreiraryan <Ryck342020@hotmail.com>
2026-03-25 10:02:55 -0300

    Feat: change key to y

--- init.lua
+++ init.lua
@@ -3,7 +3,7 @@ local M = {}
 function M.setup(opts)
   opts = opts or {}
 
-  vim.keymap.set("n", "<Leader>f", function()
+  vim.keymap.set("n", "<Leader>y", function()
     if opts.name then
       print("hello, " .. opts.name)
     else
